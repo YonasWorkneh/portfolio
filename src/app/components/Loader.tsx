2f53d46a94473cd97a97cd5bd94089019cf97f17
Yon@s W. <130277885+YonasWorkneh@users.noreply.github.com>
2025-04-17 10:22:13 +0300

    Update Loader.tsx

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -4,7 +4,7 @@ export default function Loader() {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
       <svg
-        className="w-[200px] h-[140px] stroke-[#ffffff8a] stroke-[3px] animate-draw"
+        className="size-[200px] stroke-[#ffffff8a] stroke-[3px] animate-draw"
         style={{
           fillOpacity: 0,
           strokeDasharray: 4500,
